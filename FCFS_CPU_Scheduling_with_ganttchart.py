9af835cd59d3378df5d161afc59fcef01c462d1a
Oishiqt <159407906+Oishiqt@users.noreply.github.com>
2024-02-08 13:52:56 +0800

    Delete FCFS_CPU_Scheduling_with_ganttchart.py

--- FCFS_CPU_Scheduling_with_ganttchart.py
+++
@@ -1,7 +0,0 @@
-max_length = 10
-user_input = input(f"Enter a string (maximum {max_length} characters): ")[:max_length]
-print("You entered:", user_input)
-
-# x, y = map(int, input().split())
-#
-# print(7-(x+y))
